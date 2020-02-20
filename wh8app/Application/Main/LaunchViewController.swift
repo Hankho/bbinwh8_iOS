18edf2e185fbd23efefb39b16216dc15f441d6ad
hank <hank.ho@wh8office.com>
2020-02-20 17:46:29 +0800

    create a launchController

---
+++ wh8app/Application/Main/LaunchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchViewController.swift
+//  wh8app
+//
+//  Created by 林怡廷 on 2020/2/20.
+//  Copyright © 2020 Gary Lin. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
