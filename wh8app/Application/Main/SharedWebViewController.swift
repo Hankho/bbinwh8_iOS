8a839b47a4c90ce53d1ce1c28c2cc79610dfd10a
hankho <hankho@hacker.com>
2020-02-21 16:31:40 +0800

    Add two images for troubleshooting easily.

--- wh8app/Application/Main/SharedWebViewController.swift
+++ wh8app/Application/Main/SharedWebViewController.swift
@@ -191,6 +191,12 @@ extension SharedWebViewController: WKNavigationDelegate {
         
         // Stop loading indicator on finished loading
         onIndicatorStateChanged.accept(false)
+        
+        // Check if Webview is first time loading or not
+        if (Loadwebiew.shared.isFirstTimeLoad == true){
+            ListeningWebViewSetting.shared.launchimg.removeFromSuperview()
+            Loadwebiew.shared.isFirstTimeLoad = false
+        }
     }
     
     func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
